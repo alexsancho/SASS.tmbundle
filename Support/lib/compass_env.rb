44f174d9d01ec3fe461d0369dabfc19d3d26006e
Alex Sancho <alex@alexsancho.name>
2012-03-06 01:25:54 +0100

    refactoring, splitting some modules and optimizing some regex

--- Support/lib/compass_env.rb
+++ Support/lib/compass_env.rb
@@ -3,6 +3,8 @@
 
 # Used as a common require to set up the environment for commands. 
 
+FILEPATH = "#{ENV['TM_FILEPATH']}"
+PROJECT = "#{ENV['TM_PROJECT_DIRECTORY']}"
 SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
 BUN_SUP = File.expand_path(File.dirname(__FILE__))
 DIALOG = SUPPORT + '/bin/tm_dialog'
@@ -11,10 +13,13 @@ require SUPPORT + '/lib/escape'
 require SUPPORT + '/lib/exit_codes'
 require SUPPORT + '/lib/textmate' 
 require SUPPORT + '/lib/ui'
+require SUPPORT + '/lib/web_preview'
 require SUPPORT + '/lib/tm/htmloutput'
 require SUPPORT + '/lib/osx/plist'
 
-require BUN_SUP + '/compass/log'
+require BUN_SUP + '/sass/writer'
+require BUN_SUP + '/sass/reader'
+
 require BUN_SUP + '/compass/writer'
 require BUN_SUP + '/compass/reader'
 
